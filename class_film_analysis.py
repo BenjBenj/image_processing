8bcc1210c2985e8b08be1e92883fe433431cef16
BenjBenj <you@example.com>
2018-10-16 16:32:21 +0100

    Erosion filter, bandpass filter, new threshold, length conversion, fix some bugs: progess bar, warning, etc. Now the script returns the raw film positions (along with the corresponding time)

--- class_film_analysis.py
+++ class_film_analysis.py
@@ -4,7 +4,8 @@ from scipy import signal
 class film_analysis:
 
 
-	def __init__(self, fileroot, extension, h_position, err, n_tube, w_min, w_max, v_gap):
+	def __init__(self, fileroot, extension, h_position, err, n_tube, w_min, w_max, v_gap, time_conv, \
+		low_boundary_fft_filter, high_boundary_fft_filter, threshold_value):
 		"""
 		Input: fileroot (string), extension (string), h_position (array), err (int),
 		n_tube (int), w_min (int), w_max (int), v_gap (int).
@@ -26,7 +27,10 @@ class film_analysis:
 		self.w_min = w_min
 		self.w_max = w_max
 		self.v_gap = v_gap
-
+		self.time_conv = time_conv
+		self.low_boundary_fft_filter = low_boundary_fft_filter
+		self.high_boundary_fft_filter = high_boundary_fft_filter
+		self.threshold_value = threshold_value
 
 	def open_file_4_digits(self, img_nb):
 		"""
@@ -43,47 +47,104 @@ class film_analysis:
 		return filename
 
 
+	def bandpass_filter(self, image):
+		"""
+		Input: image (matrix), low_boundary_fft_filter (float between 0 and 1),
+		high_boundary_fft_filter (float between 0 and 1).
+		-----
+		Output: filtered_image (matrix).
+		-----
+		Comment: Works out the fast Fourier transform of image, normalizes the result,
+		filters the frequencies between low_boundary_fft_filter and high_boundary_fft_filter,
+		turns the result back to the original scale, works out the inverse fast Fourier transform
+		and returns the filtered image (filtered_image).
+		-----
+		"""
+		im_fft = np.fft.rfft2(image)
+		im_fft_normalized = (im_fft - im_fft.min()) / (im_fft.max() - im_fft.min())
+		for i in range(len(im_fft_normalized[:,0])):
+			for j in range(len(im_fft_normalized[0,:])):
+				if (self.low_boundary_fft_filter > im_fft_normalized[i,j]) or (im_fft_normalized[i,j] > self.high_boundary_fft_filter):
+					im_fft_normalized[i]=0
+		im_fft_unnormalized = im_fft_normalized * (im_fft.max() - im_fft.min()) + im_fft.min()
+		filtered_image = np.fft.irfft2(im_fft_unnormalized)
+		return filtered_image
+
+
+	def threshold(self, image):
+		"""
+		Input: image (matrix), threshold_value (float between 0 and 1).
+		-----
+		Output: binary (matrix).
+		-----
+		Comment: Normalizes the grey scale, works out the threshold according to the wanted
+		value (threshold_value) and returns the binarized image (binary).
+		-----
+		"""
+		image = ((image - image.min()) / (image.max() - image.min()))
+		binary = np.copy(image)*0
+		for i in range(len(image[:,0])):
+			for j in range(len(image[0,:])):
+				if image[i,j] < self.threshold_value:
+					binary[i,j] = 0
+				else:
+					binary[i,j] = 1
+		return binary
+
+
 	def tube_analysis(self, peak_0, peak_left, peak_right):
 		"""
 		Input: peak_0 (array), peak_left (array), peak_right (array).
 		-----
-		Output: n_film (int).
+		Output: n_film (int), peak_position (array).
 		-----
 		Comment: Takes the positions of the films for the 3 measuring lines
-		(peak_0, peak_left, peak_right) and returns the number of film in the tube.
+		(peak_0, peak_left, peak_right), returns the number of films in the
+		tube (n_film) and their positions (peak_position).
 		A film is counted if the 3 measuring lines give the same values within
 		the tolerence of self.err.
 		-----
 		"""
-		real_peak = []
+		peak_position = []
 		n_film = 0
 		for j in range(len(peak_0)):
 			for k in range(len(peak_left)):
 				for l in range(len(peak_right)):
 					if (abs(peak_0[j] - peak_left[k]) <= self.err) and (abs(peak_left[k] - peak_right[l]) <= self.err):
-						real_peak.append((peak_0[j] + peak_left[k] + peak_right[l])/3)
-		n_film += len(real_peak)
-		return n_film
+						peak_position.append((peak_0[j] + peak_left[k] + peak_right[l])/3)
+		n_film += len(peak_position)
+		return n_film, peak_position
 
 
-	def image_analysis(self, binary):
+	def image_analysis(self, binary, image_nb):
 		"""
-		Input: binary (matrix).
+		Input: binary (matrix), image_nb (int).
 		-----
-		Output: n_tot (int).
+		Output: n_tot (int), all_tube_peak_position (array).
 		-----
 		Comment: Takes the full thresholded image, binary, loop over all tubes using
-		the self.tube_analysis function and returns the number of films in the image.
+		the self.tube_analysis function and returns the number of films in the image,
+		their positions and the time ([T, X, Y]).
 		-----
 		"""
 		n_tot = 0
+		all_tube_peak_position = []
+		binary = binary + 1e-16
 		for i in range(self.n_tube):
 			peak_0 = signal.find_peaks_cwt(binary[:,int(self.h_position[i])], np.arange(self.w_min, self.w_max))
 			peak_left = signal.find_peaks_cwt(binary[:,int(self.h_position[i])+self.v_gap], np.arange(self.w_min, self.w_max))
 			peak_right = signal.find_peaks_cwt(binary[:,int(self.h_position[i])-self.v_gap], np.arange(self.w_min, self.w_max))
-			n_film = self.tube_analysis(peak_0, peak_left, peak_right)
+			n_film, peak_position = self.tube_analysis(peak_0, peak_left, peak_right)
 			n_tot += n_film
-		return n_tot
+			#all_tube_peak_position.append(peak_position)
+			slice_time = np.zeros(len(peak_position)) + self.time_conv * image_nb
+			x_position = np.zeros(len(peak_position)) + self.h_position[i]
+			peak_position = np.column_stack([slice_time, x_position, peak_position])
+			if i != 0:
+				all_tube_peak_position = np.concatenate([all_tube_peak_position, peak_position], axis=0)
+			else:
+				all_tube_peak_position = peak_position
+		return n_tot, all_tube_peak_position
 
 
 	def survival_cleaning(self, survival_function):
--- main_film_analysis.py
+++ main_film_analysis.py
@@ -1,4 +1,4 @@
-from skimage import io, transform, draw, filter
+from skimage import io, transform, draw, filters, morphology
 import numpy as np
 import libconf
 import matplotlib.pyplot as plt
@@ -6,9 +6,9 @@ from class_film_analysis import film_analysis
 
 """
 The script works out the survival function from the raw images of the experiment.
-The first image is displayed, after the threshold, the rotation and the cropping.
-In addition, the measuring lines are plotted on the first image to check by eye
-the validity of the analysis.
+The first image is displayed, after the threshold, the rotation, the cropping and
+the filtering. In addition, the measuring lines are plotted on the first image
+to check by eye the validity of the analysis.
 Then, the code analyses all the images before saving the survival_function and
 the corresponding time in a text file.
 """
@@ -23,6 +23,7 @@ extension = config['extension'] # Image format
 n_image = config['n_image'] # Number of images to analyse
 incr_image = config['incr_image'] # Image increment
 time_conv = config['time_conv'] # Time conversion
+length_conv = config['length_conv'] # Pixel conversion
 time_unit = config['time_unit'] # Time unit
 err = config['err'] # Horizontal error on the detection lines
 n_tube = config['n_tube'] # Number of tubes in the experiment
@@ -31,25 +32,45 @@ w_max = config['w_max'] # Max width of the peaks
 v_gap = config['v_gap'] # Mumber of pixels between the 3 detection lines
 h_position = config['h_position'] # Horizontal position of the tubes
 rotate_angle = config['rotate_angle'] # Angle to get the image horizontal
-top = config['top'] # Start pixel left
-bottom = config['bottom'] # End pixel right
-left = config['left'] # Start pixel bottom
-right = config['right'] # End pixel top
+top = config['top'] # Start pixel top (REFERENCE FOR THE Y POSITION)
+bottom = config['bottom'] # End pixel bottom
+left = config['left'] # Start pixel left
+right = config['right'] # End pixel right
+low_boundary_fft_filter = config['low_boundary_fft_filter'] # Bandpass filter: low boundary
+high_boundary_fft_filter = config['high_boundary_fft_filter'] # Bandpass filter: high boundary
+threshold_value = config['threshold_value'] # Threshold black/white limit
 
 # Object calling
-film_ana = film_analysis(fileroot, extension, h_position, err, n_tube, w_min, w_max, v_gap)
+film_ana = film_analysis(fileroot, extension, h_position, err, n_tube, w_min, w_max, v_gap, time_conv, \
+low_boundary_fft_filter, high_boundary_fft_filter, threshold_value)
 
 # Arrays creation
 survival_function = []
 time = []
+all_film_position = [] # Raw data containing the positions of the films for each image.
 
-# Display first the image after rotation, cropping, filtering and with the position of the measuring lines
-filename = film_ana.open_file_4_digits(0)
-image = io.imread(filename)
-image = transform.rotate(image, rotate_angle, resize=True)
-image = image[top:bottom, left:right]
-thresh = filter.threshold_otsu(image)
-binary_img = image > thresh
+# Displays the first image after rotation, cropping, filtering and draws the position of the measuring lines
+# Also displays the initial number of films
+last_first_img = [int((n_image * incr_image) - 1 ), 0]
+n_last_first = np.zeros(2)
+
+for i in range(2):
+	filename = film_ana.open_file_4_digits(last_first_img[i])
+	image = io.imread(filename)
+	image = transform.rotate(image, rotate_angle, resize=True)
+	image = image[top:bottom, left:right]
+
+	### image = film_ana.bandpass_filter(image) # Filter (frequency)
+	### image = film_ana.threshold(image) # Binary threshold
+
+	thresh = filters.threshold_yen(image)
+	binary_img = image > thresh
+	binary_img = morphology.binary_erosion(binary_img, morphology.square(1))
+
+	n_last_first[i], peak_position_i = film_ana.image_analysis(binary_img, last_first_img[i])
+
+print('Initial number of films: ', int(n_last_first[1] - n_last_first[0]))
+
 for i in range(n_tube):
 	binary_img[draw.line(0, h_position[i], image.shape[0]-1, h_position[i])] = 1
 	binary_img[draw.line(0, h_position[i]+v_gap, image.shape[0]-1, h_position[i]+v_gap)] = 1
@@ -58,24 +79,43 @@ io.imshow(binary_img)
 io.show()
 
 # Image analysis, loop over all images
+print('\nProgress:\n---------\n', 0, '%')
 for i in range(n_image):
 	filename = film_ana.open_file_4_digits(i * incr_image) # Converts the image number into file name
 	image = io.imread(filename) # Opens the image
 	image = transform.rotate(image, rotate_angle, resize=True) # Rotates the image
 	image = image[top:bottom, left:right] # Crops the image
-	thresh = filter.threshold_otsu(image) # Applies an Otsu threshold
-	binary_img = image > thresh # Converts into a black and white image
-	n_tot = film_ana.image_analysis(binary_img) # Counts the number of films on the image
+
+	### image = film_ana.bandpass_filter(image) # Filter (frequency)
+	### image = film_ana.threshold(image) # Binary threshold
+
+	thresh = filters.threshold_yen(image) # Threshold
+	binary_img = image > thresh
+	binary_img = morphology.binary_erosion(binary_img, morphology.square(1)) # Removes small particles
+
+	n_tot, peak_position_i = film_ana.image_analysis(binary_img, i) # Counts the number of films on the image
 	survival_function.append(n_tot) # Fills the survival_function array with the current number of films
-	time.append(i*time_conv) # Fills the time array with the current time
+	time.append(i * time_conv) # Fills the time array with the current time
+
+	#all_film_position.append(peak_position_i)
+	if i != 0:
+		all_film_position = np.concatenate([all_film_position, peak_position_i], axis=0)
+	else:
+		all_film_position = peak_position_i
+
 	if (100 * i / n_image) % 5 == 0:
-		print(100 * i / n_image, '%')
+		print(100 * (i+1) / n_image, '%')
+
+	#print(n_tot)
+	#io.imshow(binary_img)
+	#io.show()
 
 # Survival function cleaning
 survival_function = film_ana.survival_cleaning(survival_function)
 
 # Data saving
-np.savetxt('./' + exp_id  + '_data.txt', np.transpose([time, survival_function]))
+np.savetxt('./' + exp_id  + '_survival_data.txt', np.transpose([time, survival_function]))
+np.savetxt('./' + exp_id  + '_position_data.txt', np.transpose([all_film_position[:,0], all_film_position[:,1], all_film_position[:,2] * length_conv]))
 
 # Survival function plotting
 plt.plot(time, survival_function, 'or-')
